3e7bd19429496add270101814a0d51d91dd0e316
sanjidaj <sanjidajahan341@gmail.com>
2026-03-09 22:38:15 +0600

    implement issue details modal

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const signInBtn = document.getElementById("sign-in-btn")
     }
 
  });
-}
+};
 
 const allBtn = document.getElementById("all-btn");
 const openBtn = document.getElementById("open-btn");
@@ -36,22 +36,29 @@ function clickBtn(activeBtn){
 allBtn.addEventListener("click",(event) =>{
     clickBtn(allBtn);
     loadIssues();
+    count();
 });
 openBtn.addEventListener("click",(event) =>{
     clickBtn(openBtn);
-    loadIssues();
+    loadIssues("open");
 });
 closedBtn.addEventListener("click",(event) =>{
     clickBtn(closedBtn);
-    loadIssues();
+    loadIssues("closed");
 });
 
+const allCardSection = document.getElementById("card-section");
+const totalCount = document.getElementById("total-count");
+
+function count(){
+    totalCount.innerText = allCardSection.children.length;
+};
 
 
 
  const loadingSpinner = document.getElementById("loading-spinner");
 
- const loadIssues = () => {
+ const loadIssues = (status) => {
     loadingSpinner.classList.remove("hidden");
     loadingSpinner.classList.add("flex");
     const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
@@ -59,15 +66,18 @@ closedBtn.addEventListener("click",(event) =>{
      .then((res) => res.json())
      .then(json => {
         loadingSpinner.classList.add("hidden");
-        displayIssue(json.data);
+        const filtered = status ? json.data.filter(issue => issue.status === status): json.data;
+        displayIssue(filtered);
+        count();
+       
 });
 };
  
 const displayIssue = (issues) =>{
 
     const cardSection = document.getElementById("card-section");
-    // cardSection.innerHTML = "";
-    // console.log(postContainer);
+    cardSection.innerHTML = "";
+   
 
 
 
@@ -105,7 +115,7 @@ const displayIssue = (issues) =>{
         
         const issueCard = document.createElement("div");
 
-        issueCard.innerHTML =`<div class="issue-card bg-white p-3 h-60 w-60  space-y-2 rounded-md shadow-2xl  ${borderStatus} flex flex-col ">   
+        issueCard.innerHTML =`<div class="issue-card bg-white p-3 h-60 w-60  space-y-2 rounded-md shadow-2xl  ${borderStatus} flex flex-col" >   
                 <div class="grow space-y-2">
                 <div class="flex justify-between ">
                     <img class="status w-5 h-5 rounded-full object-cover" src="${statusImg}" alt="${issue.status}">
@@ -127,9 +137,84 @@ const displayIssue = (issues) =>{
            </div>
         ` ;
         
-
+        issueCard.addEventListener("click",() => openIssueModal(issue.id) );
         cardSection.appendChild(issueCard);
 
     });
+};
+
+
+ const openIssueModal = (cardId) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${cardId}`;
+    fetch(url)
+     .then((res) => res.json())
+     .then(json => {
+        console.log("API response:", json);
+        displayDetails(json.data);
+        document.getElementById("issue-details").showModal();
+        console.log(document.getElementById("issue-details"));
+ });
+};
+const displayDetails  = (card) =>{
+
+    const singleCard = document.getElementById("single-card");
+    singleCard.innerHTML = "";
+
+       
+       const labelsBadge = card.labels.map(label => `
+      <p class="border ${label === "bug" ? "border-[#FECACA] text-[#EF4444] bg-[#FEECEC]" : "border-[#FDE68A] text-[#D97706] bg-[#FFF8DB]"} flex gap-0.5 px-2  items-center rounded-2xl text-[10px] uppercase  text-nowrap">
+      <img src="${label === "bug" ? "assets/BugDroid.png" : "assets/helpWanted.png"}" alt="${label}">
+      ${label}</p>`).join("");
+      let priorityBadge;
+      if(card.priority === "high"){
+        priorityBadge = "border  text-white bg-[#EF4444]";
+      }
+      else if(card.priority === "low"){
+        priorityBadge = "text-white bg-[#9CA3AF]";
+      }
+      else{
+         priorityBadge = "text-white bg-[#F59E0B]";
+      }
+      let statusColor ;
+      if(card.status === "open"){
+        statusColor = "bg-[#00A96E]";
+      }
+      else{
+         statusColor = "bg-[#A855F7]";
+      }
+      const date = new Date(card.createdAt).toLocaleDateString();
+     
+
+    const cardDetails = document.createElement("div");
+    cardDetails.innerHTML = `
+            <div class="modal-box space-y-3">
+                <h2 class="title font-bold text-[24px]">${card.title}</h2>
+                <div class="flex gap-2 items-center">
+                    <p class="${statusColor} text-white text-[12px] rounded-full px-2 py-1 ">${card.status}</p>
+                    <p class="assignee text-[#64748B] text-[12px]">${card.status} by ${card.assignee}</p>
+                    <time class="text-[#64748B] text-[12px]"datetime="${card.createdAt}" >${date}</time>
+                </div>
+                <div class ="space-y-2">
+                    <div class="flex gap-1">
+                    ${labelsBadge}
+                    </div>
+                    <p class="description  text-[#64748B] text-[12px] line-clamp-2">${card.description}</p>
+                </div>
+                <div class="card bg-base-300 rounded-box grid h-20 place-items-center grid-cols-2">
+                    <p>Assignee:<br> <span class="font-bold">${card.assignee}</span></p>
+                    <p>Priority:<br> <span class="text-[12px] uppercase px-2 py-1 rounded-2xl ${priorityBadge}">${card.priority}</span></p>
+                </div>
+
+                <div class="modal-action">
+                    <form method="dialog">
+                
+                        <button class="btn btn-primary">Close</button>
+                    </form>
+                </div>
+            </div>`
+
+       singleCard.appendChild(cardDetails);
+
+
 };
 loadIssues();
